799d8178700481f3036ab8226375dc819f268e56
Jordan Byrne <jordan5039@live.com>
2014-11-01 19:36:49 +0000

    first commit

--- Images/ViewController.swift
+++ Images/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var alien: UIImageView!
+    var counter = 1
+    var timer = NSTimer()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func button() {
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.085, target: self, selector: "dance", userInfo: nil, repeats: true)
+    }
 
+    @IBAction func buttone() {
+        timer.invalidate()
+    }
+    
+    func dance() {
+        counter++
+        if counter == 6 {
+            counter = 1
+        }
+        var imageName = "frame\(counter).png"
+        alien.image = UIImage(named: imageName)
+    }
 }
 
